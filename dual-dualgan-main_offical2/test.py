a69441256e016f17f4b929dd56a0d625acf66024
MCxiaoyue <142221602+MCxiaoyue@users.noreply.github.com>
2024-11-18 13:20:23 +0800

    Delete dual-dualgan-main_offical2 directory

--- dual-dualgan-main_offical2/test.py
+++
@@ -1,5 +0,0 @@
-import os
-cmd = 'python main.py --phase test --dataset_name 256voicedataset --image_size 256 --lambda_A 1000.0 --lambda_B 1000.0 --epoch 100'
-os.system(cmd)
-cmd = 'python main2.py --phase test --dataset_name 256voicedataset --image_size 256 --lambda_B 1000.0 --lambda_C 1000.0 --epoch 100'
-os.system(cmd)
